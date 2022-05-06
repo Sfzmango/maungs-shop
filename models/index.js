b3ab639eeabe14d488a3d6d2abfe12d306eea306
Maung Htike <maunghtike1999@gmail.com>
2022-05-05 22:47:38 -0700

    fix issue with belongsToManys

--- models/index.js
+++ models/index.js
@@ -10,20 +10,22 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-Category.hasMany(Products, {
+Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'category_id'
+  through: 'ProductTag',
+  foreignKey: 'tag_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'category_id'
-})
+  through: 'ProductTag',
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
