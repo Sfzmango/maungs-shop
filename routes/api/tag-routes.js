161e22c32c650e1ebc46589fe576c42fc946f295
Maung Htike <maunghtike1999@gmail.com>
2022-05-06 02:58:25 -0700

    fix typo on get id router

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+// working
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
@@ -16,24 +16,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+// working
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag }],
+      include: [{ model: Product }],
     });
 
     if (!tagData) {
       res.status(404).json({ message: 'No tags found with that id!' });
       return;
     }
-    res.status(200).json(productData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// working
 router.post('/', async (req, res) => {
   // create a new tag
   try {
@@ -44,6 +46,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+// =========================
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
@@ -62,6 +65,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// working
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
